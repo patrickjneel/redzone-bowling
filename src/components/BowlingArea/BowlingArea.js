3aa0b121fbb047bda4d94813e53487f9fb0be256
Pat Neel <patrick.neel@colorado.edu>
2018-05-04 17:27:30 -0600

    working on score functionality

--- src/components/BowlingArea/BowlingArea.js
+++ src/components/BowlingArea/BowlingArea.js
@@ -6,29 +6,34 @@ class BowlingArea extends Component {
   constructor() {
     super()
     this.state = {
-      playerName: ''
+      playerName: '',
+      scoreArr: []
     }
   }
 
-  playerName(event) {
-    console.log(event.target.value)
-   return this.setState({playerName: event.target.value})
+  // playerName(event) {
+  //   console.log(event.target.value)
+  //  return this.setState({playerName: event.target.value})
+  // }
+
+  submitScore() {
+    console.log('scoreed kljdklfadjflkd')
   }
 
   render() {
     return (
       <div className="bowling-area">
         <ScoreBoard />
-        <input placeholder="Player 1 Enter Name"
-          onChange={(event) => this.playerName(event)}
-          value={this.setState.playerName} 
-        />
-        <button 
-          onClick={(event) => this.playerName(event)}
-          value={this.setState.playerName} 
-        >
-          Submit Name
-        </button>
+        <div className="bowling-score-area">
+          <input placeholder="Player 1 Enter Name"/>
+          <button>
+            Submit Name
+          </button>
+          <input placeholder="Roll Away!!"/>
+          <button onClick={this.submitScore}>
+            Submit Score
+          </button>
+        </div>
       </div>
     )
   }
