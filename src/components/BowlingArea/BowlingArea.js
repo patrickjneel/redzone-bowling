069a33888ee43de65b6aaf9c6749a94fa743c4ac
Pat Neel <patrick.neel@colorado.edu>
2018-05-06 16:04:45 -0600

    scoring updated

--- src/components/BowlingArea/BowlingArea.js
+++ src/components/BowlingArea/BowlingArea.js
@@ -6,9 +6,24 @@ class BowlingArea extends Component {
   constructor() {
     super()
     this.state = {
+      initialPins: 11,
+      remaningPins: 10,
       playerName: '',
       playerScore: '',
-      scoreArr: []
+      scoreArr: [],
+      scoreObj: {
+        10: 0,
+        9: [0, 1]
+        8: 2,
+        7: 3,
+        6: 4,
+        5: 5,
+        4: 6,
+        3: 7,
+        2: 8,
+        1: 9,
+        0: 10 
+      }
     }
   }
 
@@ -17,35 +32,45 @@ class BowlingArea extends Component {
     this.setState({[name]: value});
   }
 
-  addScore = (event) => {
+  addScore = (event) => {  
     if(this.state.scoreArr.length < 20){
-      const scoreArr = [...this.state.scoreArr, this.state.playerScore];
-      this.setState({ scoreArr });
-      this.setState({ playerScore: ''});
+
+      if(this.state.scoreArr.length % 2 === 0) {
+        const randomNum = Math.floor(Math.random() * Math.floor(this.state.initialPins))
+        const scoreArr = [...this.state.scoreArr, randomNum];
+        this.setState({ remaningPins: randomNum, scoreArr })
+          console.log(scoreArr)
+      }
+
+      if(this.state.scoreArr.length % 2 !== 0) {
+        const remaningTest = Math.floor(Math.random() * Math.floor(this.state.scoreObj[this.state.remaningPins]));
+
+
+        const scoreArr = [...this.state.scoreArr, remaningTest];
+        this.setState({ scoreArr });
+          console.log(scoreArr)
+      }
+
+        
+
+
+
+      // const scoreArr = [...this.state.scoreArr, this.state.playerScore];
+      // this.setState({ scoreArr });
+      // this.setState({ playerScore: ''});
     }
     event.preventDefault();
   }
 
-  gameOver = () => {
-      if(this.state.scoreArr.length > 20) {
-        return (
-          <div>
-           Game Over
-          </div>
-        )
-      }
-    }
-
   render() {
+    console.log(this.state.scoreArr)
     return (
       <div className="bowling-area">
         <ScoreBoard 
           name={this.state.playerName}
           scoreArray={this.state.scoreArr}
         />
-        {
-          this.gameOver()
-        }
+       
         <div className="bowling-score-area">
         <div className="rules">
           Enter a Number 0-10 button is disabled if a number over 10 input or less than 0
@@ -69,11 +94,11 @@ class BowlingArea extends Component {
             />
             <button 
               onClick={this.addScore}
-              disabled={
-                this.state.playerScore > 10 || 
-                this.state.playerScore < 0 ||
-                this.state.playerScore === '' 
-              }
+              // disabled={
+              //   this.state.playerScore > 10 || 
+              //   this.state.playerScore < 0 ||
+              //   this.state.playerScore === '' 
+              // }
             >
               Submit Score
             </button>
