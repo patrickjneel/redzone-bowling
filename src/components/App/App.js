130156411414aabb917e921febe60597f46f04ab
Pat Neel <patrick.neel@colorado.edu>
2018-05-07 15:45:14 -0600

    refactored App component into own folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import BowlingArea from './components/BowlingArea/BowlingArea';
+import BowlingArea from '../BowlingArea/BowlingArea';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img className="logo-img" src={require('./redzone.png')} alt="redzone-logo"/>
+          <img className="logo-img" src={require('../../assets/redzone.png')} alt="redzone-logo"/>
           <h1 className="App-title">BOWLING</h1>
         </header>
-        <img className="bowling-img" src={require('./bowl.jpg')} />
+        <img className="bowling-img" src={require('../../assets/bowl.jpg')} />
         <BowlingArea />
       </div>
     );
