aaa9f4120df86c9258d3999a62e970f8a91a4951
Pat Neel <patrick.neel@colorado.edu>
2018-05-07 14:04:35 -0600

    added set up for testing and wrote one test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it('should match the snap shot', () => {
+    const renderedApp = shallow(<App />);
+    expect(renderedApp).toMatchSnapshot();
+
+  });
